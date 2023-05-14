d715037eb1d27320d59d59283133cff06cd6023d
Geoffrey Smith <fork@tysontechnology.com.au>
2023-05-15 01:14:07 +1000

    Add function to allow Microsoft Face Recognition to use a local file or stream

--- Libs/FaceRecognition/uMicrosoft.FaceRecognition.pas
+++ Libs/FaceRecognition/uMicrosoft.FaceRecognition.pas
@@ -34,13 +34,70 @@ begin
 end;
 
 function TMicrosoftFaceRecognition.DetectFacesFromFile(imageFilename: string): string;
+var
+  fs : TFileStream;
 begin
-
+  fs := TFileStream.Create(imageFilename, fmOpenRead);
+  try
+    Result := DetectFacesFromStream(fs);
+  finally
+    FreeAndNil(fs);
+  end;
 end;
 
 function TMicrosoftFaceRecognition.DetectFacesFromStream(imageStream: TStream): string;
+var
+  restClient: TRESTClient;
+  restRequest: TRESTRequest;
+  restResponse: TRESTResponse;
+  serviceUrl: string;
+  uri : TURI;
+  response : TJSONArray;
 begin
+  restClient := nil;
+  restRequest := nil;
+  restResponse := nil;
+  try
+    restClient := TRESTClient.Create(nil);
+    restRequest := TRESTRequest.Create(nil);
+    restResponse := TRESTResponse.Create(nil);
+    // Construct the API endpoint URL
+  //  url := 'https://<your region>.api.cognitive.microsoft.com/face/v1.0/detect';
+    serviceUrl := 'https://adugfaces.cognitiveservices.azure.com/face/v1.0/detect';
+    uri := TURI.Create(serviceUrl);
+    uri.AddParameter('returnFaceId', 'false');
+    uri.AddParameter('returnFaceLandmarks', 'true');
 
+    // Set the necessary REST client properties
+    restClient.BaseURL := uri.ToString;
+    restClient.ContentType := 'application/json';
+    restClient.HandleRedirects := True;
+    restClient.Authenticator := nil; // no authentication required for the Face API
+
+    // Set the necessary REST request properties
+    restRequest.Method := TRESTRequestMethod.rmPOST;
+    restRequest.AddBody(imageStream, TRESTContentType.ctAPPLICATION_OCTET_STREAM);
+    restRequest.Client := restClient;
+    restRequest.Response := restResponse;
+
+    // Set the necessary REST request headers
+    restRequest.Params.AddHeader('Ocp-Apim-Subscription-Key', FAPIKey);
+
+    // Execute the REST request and get the response
+    restRequest.Execute;
+
+    // Parse the JSON response and return the array of detected faces
+    response := (TJSONObject.ParseJSONValue(restResponse.Content) as TJSONArray);
+    try
+      Result := response.ToJSON;
+    finally
+      FreeAndNil(response);
+    end;
+  finally
+    FreeAndNil(restResponse);
+    FreeAndNil(restRequest);
+    FreeAndNil(restClient);
+  end;
 end;
 
 function TMicrosoftFaceRecognition.DetectFacesFromURL(imageUrl: string): string;
@@ -53,10 +110,10 @@ var
   request: TJSONObject;
   response : TJSONArray;
 begin
-    restClient := nil;
-    restRequest := nil;
-    restResponse := nil;
-    request := nil;
+  restClient := nil;
+  restRequest := nil;
+  restResponse := nil;
+  request := nil;
   try
     restClient := TRESTClient.Create(nil);
     restRequest := TRESTRequest.Create(nil);
