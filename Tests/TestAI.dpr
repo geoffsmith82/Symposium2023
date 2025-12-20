d7510ef4288f48436504e14e6666a5f97d517fcf
Geoffrey Smith <geoff@tyson.technology>
2025-12-21 01:47:32 +1100

    Add files to TestAI project

--- Tests/TestAI.dpr
+++ Tests/TestAI.dpr
@@ -15,7 +15,36 @@ uses
   DUnitX.Loggers.Xml.NUnit,
   {$ENDIF }
   DUnitX.TestFramework,
-  Unit1 in 'Unit1.pas';
+  TestBase.AI in 'TestBase.AI.pas',
+  TestBase.LLM in 'TestBase.LLM.pas',
+  TestBase.ImageGen in 'TestBase.ImageGen.pas',
+  Test.LLM.Groq in 'Test.LLM.Groq.pas',
+  Test.LLM.OpenAI in 'Test.LLM.OpenAI.pas',
+  Test.ImageGen.Replicate in 'Test.ImageGen.Replicate.pas',
+  uAttributes in '..\Libs\LLM\uAttributes.pas',
+  uLLM.Anthropic in '..\Libs\LLM\uLLM.Anthropic.pas',
+  uLLM.Azure in '..\Libs\LLM\uLLM.Azure.pas',
+  uLLM.DeepSeek in '..\Libs\LLM\uLLM.DeepSeek.pas',
+  uLLM.Functions in '..\Libs\LLM\uLLM.Functions.pas',
+  uLLM.Google.Gemini in '..\Libs\LLM\uLLM.Google.Gemini.pas',
+  uLLM.Google.PaLM in '..\Libs\LLM\uLLM.Google.PaLM.pas',
+  uLLM.Groq in '..\Libs\LLM\uLLM.Groq.pas',
+  uLLM.HuggingFace in '..\Libs\LLM\uLLM.HuggingFace.pas',
+  uLLM.Mistral in '..\Libs\LLM\uLLM.Mistral.pas',
+  uLLM.OpenAI.Assistants in '..\Libs\LLM\uLLM.OpenAI.Assistants.pas',
+  uLLM.OpenAI in '..\Libs\LLM\uLLM.OpenAI.pas',
+  uLLM.OpenRouter in '..\Libs\LLM\uLLM.OpenRouter.pas',
+  uLLM in '..\Libs\LLM\uLLM.pas',
+  uLLM.Replicate in '..\Libs\LLM\uLLM.Replicate.pas',
+  uLLM.X.Ai in '..\Libs\LLM\uLLM.X.Ai.pas',
+  uDALLe2.DTO in '..\Libs\ImageGeneration\uDALLe2.DTO.pas',
+  uImageGeneration.Imagen in '..\Libs\ImageGeneration\uImageGeneration.Imagen.pas',
+  uImageGeneration.OpenAI in '..\Libs\ImageGeneration\uImageGeneration.OpenAI.pas',
+  uImageGeneration in '..\Libs\ImageGeneration\uImageGeneration.pas',
+  uImageGeneration.Replicate in '..\Libs\ImageGeneration\uImageGeneration.Replicate.pas',
+  uImageGeneration.XAI in '..\Libs\ImageGeneration\uImageGeneration.XAI.pas',
+  ApiKeyStore in '..\Libs\ApiKeyStore\ApiKeyStore.pas',
+  ApiKeyStore.Windows in '..\Libs\ApiKeyStore\ApiKeyStore.Windows.pas';
 
 { keep comment here to protect the following conditional from being removed by the IDE when adding a unit }
 {$IFNDEF TESTINSIGHT}
