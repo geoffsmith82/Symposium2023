a3db773653630d9a6a37564498f6e0c76569367f
Geoffrey Smith <geoff@tyson.technology>
2025-12-20 15:48:49 +1100

    Update Amazon Transcribe

--- Libs/SpeechToText/uSTT.Amazon.pas
+++ Libs/SpeechToText/uSTT.Amazon.pas
@@ -24,12 +24,9 @@ type
 
 implementation
 
-{$IFNDEF NOPOLLY}
 uses
-  AWS.Transcribe,  // need to install AWS SDK For Delphi Preview
-  AWS.S3,
-  AWS.Core;    // Need Delphi Enterprise or above and install from
-{$ENDIF}              // GetIt
+  AWS.Transcribe,
+  AWS.S3;
 
 { TAmazonSpeechToText }
 
@@ -50,40 +47,38 @@ end;
 
 function TAmazonSpeechToText.TranscribeAudio(const FilePath, ModelName: string): string;
 var
-  transcribeClient : TTranscribeClient;
-  s3Client : IS3Client;
-  request : TTranscribeStartTranscriptionJobRequest;
-  response : ITranscribeResponse;
-  response2 : ITranscribeGetTranscriptionJobResponse;
-  putObject : TS3PutObjectRequest;
-  getObject : TS3GetObjectRequest;
-  putObjectResponse: IS3PutObjectResponse;
-  getObjectResponse: IS3GetObjectResponse;
-  media : ITranscribeMedia;
-  options : IAWSOptions;
+  transcribeClient : TAmazonTranscribeServiceClient;
+  s3Client : TAmazonS3Client;
+  request : IStartTranscriptionJobRequest;
+  request2 : IGetTranscriptionJobRequest;
+  response : IStartTranscriptionJobResponse;
+  response2 : IGetTranscriptionJobResponse;
+  putObject : IPutObjectRequest;
+  getObject : TGetObjectRequest;
+  putObjectResponse: IPutObjectResponse;
+  getObjectResponse: TGetObjectResponse;
+  media : TMedia;
   stream : TFileStream;
   key : string;
 begin
-  options := TAWSOptions.Create;
-  options.AccessKeyId := FAccessKey;
-  options.SecretAccessKey := FSecretKey;
-  options.Region := FRegion;
-  transcribeClient := TTranscribeClient.Create(options);
-  s3Client := TS3Client.Create(options);
+  transcribeClient := TAmazonTranscribeServiceClient.Create(FAccessKey, FSecretKey, FRegion);
+  s3Client := TAmazonS3Client.Create(FAccessKey, FSecretKey, FRegion);
   stream := nil;
   try
     stream := TFileStream.Create(FilePath, fmOpenRead);
-    putObject := TS3PutObjectRequest.Create(FBucket, Key, stream);
+    putObject := TPutObjectRequest.Create;
+    //(FBucket, Key, stream);
     putObjectResponse := s3Client.PutObject(putObject);
 //    getObject := TS3GetObjectRequest.Create(FBucket, Key);
 //    getObjectResponse := s3Client.GetObject(getObject);
 //    s3Client.
 
-    media := TTranscribeMedia.Transcribe('s3uri');
-    request := TTranscribeStartTranscriptionJobRequest.Create('Job', media);
+    media := TMedia.Create;
+//    media.MediaFileUri :=
+    request := TStartTranscriptionJobRequest.Create;//('Job', media);
     response := transcribeClient.StartTranscriptionJob(request);
 
-    response2 := transcribeClient.GetTranscriptionJob('Job');
+    response2 := transcribeClient.GetTranscriptionJob(request2);
 
  //   response2.TranscriptionJob.Transcript.TranscriptFileUri
 
